0fa19b3c41a33f6a42dadb1df0b7c36edda20b60
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-08-19 13:22:40 +0200

    added about screen

---
+++ screens/about.js
@@ -0,0 +1,13 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function About() {
+  return(
+    <View>
+      <Text>AboutScreen</Text>
+      <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </Text>
+    </View>
+  )
+}
