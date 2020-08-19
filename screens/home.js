d754aca9ed7b92f239924c744e3258eda602782c
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-08-19 13:21:26 +0200

    added home screen

---
+++ screens/home.js
@@ -0,0 +1,9 @@
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, FlatList, TouchableOpacity, Modal, TouchableWithoutFeedback, Keyboard } from 'react-native';
+import { globalStyles } from '../styles/global';
+
+export default function Home() {
+  return (
+    <View>HomeScreen</View>
+  );
+} 
