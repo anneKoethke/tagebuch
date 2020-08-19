37fc6afbfa6374a00b18c908109c2109a604ed36
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-08-19 13:26:19 +0200

    added globalStyles: container,titleText,paragraph

--- shared/global.js
+++ shared/global.js
@@ -1,5 +1,17 @@
 import { StyleSheet } from 'react-native';
 
 export const globalStyles = StyleSheet.create({
-  
+  container: {
+    padding: 20,
+    flex: 1
+  },
+  titleText: {
+    //fontFamily: 'nunito-bold',
+    fontSize: 18,
+    color: '#333'
+  },
+  paragraph: {
+    marginVertical: 8,
+    lineHeight: 20
+  },
 });
