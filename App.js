6b35df018473cc2fe26e30ace9e5c0595fb42fd5
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-08-25 09:57:17 +0200

    fonts and fonts loading added

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo'; 
+
+const getFonts = () => Font.loadAsync({
+  'permanent-marker': require('./assets/fonts/Permanent_Marker/PermanentMarker-Regular.ttf'),
+  'kalam-light': require('./assets/fonts/Kalam/Kalam-Light.ttf')
+});
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Tagebuch</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+  const [fontsLoaded, setFontsLoaded] = useState(false);
+  
+  if(fontsLoaded) {
+    return (
+      <View >
+        <Text>Tagebuch</Text>
+        <StatusBar style="auto" />
+      </View>
+    );
+  } else {
+    return (
+      <AppLoading 
+        startAsync={getFonts}
+        onFinish={() => setFontsLoaded(true)}
+      />
+    );
+  }
+  
+}
--- screens/about.js
+++ screens/about.js
@@ -7,7 +7,7 @@ export default function About() {
   return(
     <View>
       <Text>AboutScreen</Text>
-      <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. </Text>
+      <Text>Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua.</Text>
     </View>
   )
 }
