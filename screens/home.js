b1aee4c9540f2d7a7651a0babed3b3a86e4f4cad
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-08-25 11:02:40 +0200

    added pseudo content to home

--- screens/home.js
+++ screens/home.js
@@ -4,6 +4,13 @@ import { globalStyles } from '../styles/global';
 import Header from '../shared/header';
 
 export default function Home() {
+
+  const [entries, setEntries] = useState([
+    {title: "Today was boring", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugiat adipisci optio sed numquam corrupti natus odit, earum repellat dicta consectetur rem maxime odio suscipit, id voluptatibus. Similique nemo sapiente aperiam, vel ad magni, eaque porro odit sit doloremque totam sed eligendi tempore placeat, repellendus officiis facilis quis explicabo vitae recusandae.", mood: "2", key: "1598345947014"}, 
+    {title: "Still boring as f...", content: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto ab non ad qui, ullam fuga? Tenetur harum assumenda numquam doloremque!", mood: "1", key: "1598345960221"},
+    {title: "got slightly better", content: "Similique nemo sapiente aperiam, vel ad magni, eaque porro odit sit doloremque totam sed eligendi tempore placeat, repellendus officiis facilis quis explicabo vitae recusandae.", mood: "3", key: "1598345960221"},
+  ])
+
   return (
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>Montag</Text>
