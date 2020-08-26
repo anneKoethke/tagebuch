76d28ba19d55c6e1c122c180296bb5e643197096
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-08-26 13:10:43 +0200

    new about text

--- screens/about.js
+++ screens/about.js
@@ -1,14 +1,31 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Linking } from 'react-native';
 import { globalStyles } from '../styles/global';
 
 export default function About() {
+  
+  const handlePress = () => {
+    Linking.openURL("https://www.youtube.com/watch?v=ur6I5m2nTvk&list=PL4cUxeGkcC9ixPU-QkScoRBVxtPPzVjrQ&index=1");
+  };
+
   return(
     <View style={globalStyles.container}>
       <Text style={globalStyles.titleText}>About this App</Text>
-      <Text style={globalStyles.paragraph}>Hello and Welcome, friends!</Text>
-      <Text style={globalStyles.paragraph}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo nihil veniam ratione fugit odit fugiat nulla explicabo illum! Necessitatibus, eaque quisquam vero itaque aliquid numquam neque et ullam quas. Laborum sapiente, consectetur enim sit dolor, reprehenderit ipsum nostrum unde incidunt praesentium quasi pariatur qui quo laboriosam repudiandae magni doloremque soluta.</Text>
+      <Text style={[globalStyles.first, globalStyles.paragraph]}>Hello and Welcome, friends!</Text>
+      <Text style={globalStyles.paragraph}>
+        This is my version of the CodeNinja's React Native Tutorial 
+        <Text style={[styles.link, globalStyles.paragraph]} onPress={handlePress}> on YouTube </Text>
+        with some changes (due to RN developement) and some addOns (because I want to do more stuff here, e.g. unique item.key via timestamp).
+      </Text>
+      <Text style={globalStyles.paragraph}>This is not planned for release, so don't even ask about iPhone... Apple makes me instantly want to give a new bad mood entry in this app :P </Text>
+      
     </View>
   )
 }
+
+const styles = StyleSheet.create({
+  link: {
+    color: 'blue'
+  }
+})
