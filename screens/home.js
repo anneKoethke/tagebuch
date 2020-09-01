d312e1ab50b74ab49fa422d3e311bc8fda1b7e63
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-01 11:02:18 +0200

    changed todo

--- screens/home.js
+++ screens/home.js
@@ -27,7 +27,7 @@ export default function Home({ navigation }) {
     expo install react-native-gesture-handler react-native-reanimated react-native-screens react-native-safe-area-context @react-native-community/masked-view
   */   
 
-  //TODO: (maybe in real RN App) save on device for persistency
+  //TODO: (real RN App) save on device for persistency
 
   return (
     <View style={globalStyles.container}>
