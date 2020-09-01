382d2c093a8038cb4c4764b14ed65ba3880bc286
anneKoethke <anne.koethke@stud.uni-regensburg.de>
2020-09-01 11:03:55 +0200

    changed mood input to selection by picture (no fb)

--- screens/entryForm.js
+++ screens/entryForm.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import { StyleSheet, View, Text, TextInput, Button } from 'react-native';
-import { globalStyles } from '../styles/global';
+import React, { useState } from 'react';
+import { StyleSheet, ScrollView, View, Text, TextInput, Button, Image, TouchableOpacity } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import { Formik } from 'formik';
 import * as yup from 'yup';
 import FlatButton from '../shared/button';
@@ -15,12 +15,15 @@ const entrySchema = yup.object({
   // TODO: mood input und Regel
   mood: yup.string()
     .required()
-    .test('is-num-1-5', 'Mood must be a number from 1 to 5', (val) => {
+    .test('mood-selected', 'Mood must be selected', (val) => {
       return parseInt(val) < 6 && parseInt(val) > 0;
     })
 });
 
 export default function EntryForm({ addEntry }) {
+
+  //const [selectedValue, setSelectedValue] = useState('3');
+
   return(
     <View style={globalStyles.container}>
       <Formik
@@ -29,11 +32,12 @@ export default function EntryForm({ addEntry }) {
         onSubmit={(values, actions) => {
           actions.resetForm();
           addEntry(values);
-          //console.log(values);
+          console.log(values);
         }}
       >
         {props => (
-          <View>
+          <ScrollView>
+            <Text style={styles.innerHeading}>Choose your Title and Content</Text>
             <TextInput
               style={globalStyles.input}
               placeholder='Entry Title'
@@ -54,8 +58,8 @@ export default function EntryForm({ addEntry }) {
               onBlur={props.handleBlur('content')}
             />
             <Text style={globalStyles.errorText}>{ props.touched.content && props.errors.content }</Text>
-            {/* TODO: fix mood input and schema accordingly*/}
-            <TextInput 
+            
+            {/* <TextInput 
               style={globalStyles.input}
               placeholder='Mood (1 - 5)'
               onChangeText={props.handleChange('mood')}
@@ -63,14 +67,86 @@ export default function EntryForm({ addEntry }) {
               keyboardType='numeric'
               onBlur={props.handleBlur('mood')} 
             />
+            <Text style={globalStyles.errorText}>{ props.touched.mood && props.errors.mood }</Text>*/}
+            
+            {/* TODO: images with text to select */}
+
+            <Text style={styles.innerHeading}>Choose your mood:</Text>
+
+            <View style={styles.moodImageContainer}>
+              <View style={styles.moodView}>
+                <TouchableOpacity 
+                  style={[ styles.imageStyle ]}
+                  onPress={() => { props.values.mood= '1'; props.handleChange('mood') }}>
+                  <Image source={images.moods[1]} />
+                </TouchableOpacity>
+                <Text style={globalStyles.paragraph}>angry</Text>
+              </View>
+              <View style={styles.moodView}>
+                <TouchableOpacity 
+                  style={[ styles.imageStyle ]}
+                  onPress={() => { props.values.mood= '2'; props.handleChange('mood') }}>
+                  <Image source={images.moods[2]} />
+                </TouchableOpacity>
+                <Text style={globalStyles.paragraph}>sad</Text>
+              </View>
+              <View style={styles.moodView}>
+                <TouchableOpacity 
+                  style={[ styles.imageStyle ]}
+                  onPress={() => { props.values.mood= '3'; props.handleChange('mood') }}>
+                  <Image source={images.moods[3]} />
+                </TouchableOpacity>
+                <Text style={globalStyles.paragraph}>neutral</Text>
+              </View>
+              <View style={styles.moodView}>
+              <TouchableOpacity 
+                  style={[ styles.imageStyle ]}
+                  onPress={() => { props.values.mood= '4'; props.handleChange('mood') }}>
+                <Image source={images.moods[4]} />
+              </TouchableOpacity>
+                <Text style={globalStyles.paragraph}>happy</Text>
+              </View>
+              <View style={styles.moodView}>
+                <TouchableOpacity 
+                  style={[ styles.imageStyle ]}
+                  onPress={() => { props.values.mood= '5'; props.handleChange('mood') }}>
+                  <Image source={images.moods[5]} />
+                </TouchableOpacity>
+                <Text style={globalStyles.paragraph}>overjoyed</Text>
+              </View>
+            </View>
             <Text style={globalStyles.errorText}>{ props.touched.mood && props.errors.mood }</Text>
             
             <FlatButton text='submit' onPress={props.handleSubmit} /> 
-          </View>
+          </ScrollView>
         )}
 
       </Formik>
-
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  innerHeading: {
+    fontFamily: 'kalam-bold',
+    fontSize: 25,
+    lineHeight: 25,
+    paddingVertical: 25,
+  },
+  moodImageContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  moodView: {
+    flexDirection: 'column',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+  },
+  imageStyle: {
+    borderWidth: 1,
+    borderColor: '#ddd',
+    padding: 10,
+    borderRadius: 6,
+  }
+})
